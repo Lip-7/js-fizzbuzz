116d200bf9c611944dc2fb2dafdf4cb7d3d395af
Lip-7 <zhighofficial@gmail.com>
2023-03-10 16:24:32 +0100

    done the main project

--- js/functions.js
+++ js/functions.js
@@ -1,6 +1,24 @@
 function switchVisibility(value1,value2){
-    const content1 = document.getElementById(value1)
-    const content2 = document.getElementById(value2)
-    content1.classList.add('d-none')
-    content2.classList.remove('d-none')
+    const content1 = document.getElementById(value1);
+    const content2 = document.getElementById(value2);
+    content1.classList.add('d-none');
+    content2.classList.remove('d-none');
 }
+function squareGenerator(squaresNumber){
+    const fizz = 3
+    const buzz = 5
+    iterable = parseInt(squaresNumber);
+    squareLocation = document.getElementById('result-container');
+    document.getElementById('result-container').innerHTML += `<div class="single-box d-flex align-items-center justify-content-center"><span><a href="./index.html">Restart</a></span></div>`
+    for (iterable; iterable > 0; iterable--) {
+        if (iterable % fizz == 0 && iterable % buzz == 0) {
+            document.getElementById('result-container').innerHTML += `<div class="single-box d-flex align-items-center justify-content-center lip-bg-buzzfizz"><span>FizzBuzz</span></div>`
+        }else if (iterable % buzz == 0){
+            document.getElementById('result-container').innerHTML += `<div class="single-box d-flex align-items-center justify-content-center lip-bg-buzz"><span>Buzz</span></div>`
+        }else if (iterable % fizz == 0) {
+            document.getElementById('result-container').innerHTML += `<div class="single-box d-flex align-items-center justify-content-center lip-bg-fizz"><span>Fizz</span></div>`
+        }else{
+            document.getElementById('result-container').innerHTML += `<div class="single-box d-flex align-items-center justify-content-center lip-bg-normal"><span>${iterable}</span></div>`
+        }
+    };
+};
--- js/script.js
+++ js/script.js
@@ -1,2 +1,11 @@
-document.getElementById('btn-start').addEventListener('click', function(){switchVisibility('btn-start','squareCount-display')})
+/*Variables*/
+var squaresNumber = 0
 
+
+/*Buttons sheet*/
+document.getElementById('btn-start').addEventListener('click', function(){switchVisibility('btn-start','squareCount-display')})
+document.getElementById('btnGenerate').addEventListener('click', function(){
+    squaresNumber = document.getElementById('squareCount').value
+    switchVisibility('squareCount-display', 'result-wrap')
+    squareGenerator(squaresNumber)
+})
