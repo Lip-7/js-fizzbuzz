15655abdb4a89e0a1bce5da6c13e28155f47ce7c
Lip-7 <zhighofficial@gmail.com>
2023-03-10 21:12:24 +0100

    added transitions

--- js/functions.js
+++ js/functions.js
@@ -9,6 +9,9 @@ function switchVisibility2(value1,value2){
     const content2 = document.getElementById(value2);
     content1.classList.add('d-none');
     content2.classList.add('show');
+    setTimeout(() => {
+        content2.style.opacity = 1;
+      }, 10);
 }
 
 function squareGenerator(squaresNumber){
@@ -17,15 +20,24 @@ function squareGenerator(squaresNumber){
     iterable = parseInt(squaresNumber);
     squareLocation = document.getElementById('result-container');
     for (let i = 1; i <= iterable; i++) {
-        if (i % fizz == 0 && i % buzz == 0) {
-            document.getElementById('result-container').innerHTML += `<div class="single-box d-flex align-items-center justify-content-center lip-bg-buzzfizz"><span>FizzBuzz</span></div>`
-        }else if (i % buzz == 0){
-            document.getElementById('result-container').innerHTML += `<div class="single-box d-flex align-items-center justify-content-center lip-bg-buzz"><span>Buzz</span></div>`
-        }else if (i % fizz == 0) {
-            document.getElementById('result-container').innerHTML += `<div class="single-box d-flex align-items-center justify-content-center lip-bg-fizz"><span>Fizz</span></div>`
-        }else{
-            document.getElementById('result-container').innerHTML += `<div class="single-box d-flex align-items-center justify-content-center lip-bg-normal"><span>${i}</span></div>`
-        }
+        setTimeout(() => {
+            let newSquare = document.createElement('div');
+            newSquare.classList.add('single-box', 'd-flex', 'align-items-center', 'justify-content-center');
+            if (i % fizz == 0 && i % buzz == 0) {
+                newSquare.classList.add('lip-bg-buzzfizz');
+                newSquare.innerHTML = '<span>FizzBuzz</span>';
+            }else if (i % buzz == 0){
+                newSquare.classList.add('lip-bg-buzz');
+                newSquare.innerHTML = '<span>Buzz</span>';
+            }else if (i % fizz == 0) {
+                newSquare.classList.add('lip-bg-fizz');
+                newSquare.innerHTML = '<span>Fizz</span>';
+            }else{
+                newSquare.classList.add('lip-bg-normal');
+                newSquare.innerHTML = `<span>${i}</span>`;
+            }
+            squareLocation.appendChild(newSquare);
+        }, i * 20);
     };
-    document.getElementById('result-container').innerHTML += `<div class="single-box d-flex align-items-center justify-content-center"><span><a href="./index.html">Restart</a></span></div>`
+    document.getElementById('result-container').innerHTML += `<div class="single-box d-flex align-items-center justify-content-center order-1"><span><a href="./index.html">Restart</a></span></div>`
 };
