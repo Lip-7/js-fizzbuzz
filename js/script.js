e458c672ef300f852f8ae7b7b64b50f677b56f6a
Lip-7 <zhighofficial@gmail.com>
2023-03-11 12:11:41 +0100

    minor fixes

--- js/script.js
+++ js/script.js
@@ -1,8 +1,19 @@
 /*Variables*/
-var squaresNumber = 0
+var squaresNumber = 0;
+const numberInput = document.getElementById('squareCount');
 
 
 /*Buttons sheet*/
+numberInput.addEventListener("input", function() {
+    const button = document.getElementById("btnGenerate");
+    if (parseInt(numberInput.value) > 9) {
+      button.style.opacity = '0'
+      button.style.opacity = '1'
+    } else {
+      button.style.opacity = '1'
+      button.style.opacity = '0'
+    }
+  });
 document.getElementById('btn-start').addEventListener('click', function(){switchVisibility('btn-start','squareCount-display')})
 document.getElementById('btnGenerate').addEventListener('click', function(){
     squaresNumber = parseInt(document.getElementById('squareCount').value)
